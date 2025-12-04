3fb9607174c1c2ee1e0e50aca6ab547288e22c83
louixd <louis.poleym@gmail.com>
2025-12-04 08:54:31 +0000

    backup

---
+++ script.js
@@ -0,0 +1,6 @@
+let monElement = document.querySelector('specs');
+monElement.addEventListener('click', () => {
+   // Le code à exécuter au clic
+});
+// Ajouter/Enlever une classe 
+document.body.classList.toggle('specs');

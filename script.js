3a9aa6b57de50e9c9ccca84f6e7db138eb3e5dc6
louixd <louis.poleym@gmail.com>
2025-12-09 15:43:56 +0000

    final git

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let cacher = document.querySelector('#cacher');
-document.querySelector('Fiches')
+const specs = document.querySelector('#specs');
+document.querySelector('#cacher');
+document.querySelector('specs')
 if (cacher) {
 
-  cacher.addEventListener('click', () => {
-  
-    document.body.classList.toggle('Fiches');
+   cacher.addEventListener('click', () => {
+   document.body.classList.toggle('cache');
   });
 }
+
+

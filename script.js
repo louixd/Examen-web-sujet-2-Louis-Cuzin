9455fb8513ca03cc3191de5fca97f813ae0980f4
louixd <louis.poleym@gmail.com>
2025-12-09 14:34:04 +0000

    bouton invisible

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let monElement = document.querySelector('specs');
-monElement.addEventListener('click', () => {
-   // Le code à exécuter au clic
-});
-// Ajouter/Enlever une classe 
-document.body.classList.toggle('specs');
+let cacher = document.querySelector('#cacher');
+document.querySelector('Fiches')
+if (cacher) {
+
+  cacher.addEventListener('click', () => {
+  
+    document.body.classList.toggle('Fiches');
+  });
+}
